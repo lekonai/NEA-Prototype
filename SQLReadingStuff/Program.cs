d3dc2529929b92ce5be5611c79344e794fa78077
lekonai <lekonai@users.noreply.github.com>
2024-06-15 23:28:45 +0100

    cleaning it up.

--- SQLReadingStuff/Program.cs
+++ SQLReadingStuff/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace SQLReadingStuff
+﻿using System.Data.SQLite;
+
+namespace SQLReadingStuff
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* arrange for loop that goes through days:
+             * DaySpecificKey.DayID = n <--- value to loop through 
+             * DaySpecificKey.DayName <--- value to print
+             * SELECT TaskName
+             * (additionally, there's a lot more you can add.
+             * FROM TaskType, DaySpecificKey, WeeklyAllocated
+             * WHERE DaySpecificKey.DayID = n AND WeeklyAllocated = n
+             * ORDER BY TaskType.ID asc
+             *
+             *
+            */
+            SQLiteConnection conn = CreateConn();
+
+        }
+        static SQLiteConnection CreateConn()
+        {
+            SQLiteConnection theConnection;
+            theConnection = new SQLiteConnection("Data Source = TasksFilled.db; Version = 3; New = True; Compress = True;"); // MAKE SURE TO CHANGE NAME HERE. tasksfilled is a placeholder name
+            theConnection.Open();
+            return theConnection;
         }
     }
 }
