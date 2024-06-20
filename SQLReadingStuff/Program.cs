dcc93364992700886373f0dbff31eb95dadd17fe
23OC0183 <lifeaddlife@gmail.com>
2024-06-20 18:00:03 +0100

    finsihed

--- SQLReadingStuff/Program.cs
+++ SQLReadingStuff/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.SQLite;
+using System.Diagnostics;
 
 namespace SQLReadingStuff
 {
@@ -18,6 +19,22 @@ namespace SQLReadingStuff
              *
             */
             SQLiteConnection conn = CreateConn();
+            ConsoleKeyInfo autoChoice;
+            do
+            {
+                Console.WriteLine("put in auto tasks? y/n");
+                autoChoice = Console.ReadKey();
+                Console.WriteLine();
+            } while(autoChoice.Key != ConsoleKey.Y || autoChoice.Key != ConsoleKey.N);
+            if (autoChoice.Key == ConsoleKey.Y)
+            {
+                necessitiesTaskAutoSetup(conn);
+            }
+            mainMenu(conn);
+        }
+
+        static void mainMenu(SQLiteConnection conn)
+        {
             ConsoleKeyInfo choiceK;
             do
             {
@@ -36,7 +53,7 @@ namespace SQLReadingStuff
         static void getMenuChoice()
         {
             Console.Clear();
-            Console.WriteLine("Read [1]\nNew Task[2]\nDelete Task[3]");
+            Console.WriteLine("[1] Read\n[2] New Task\n[3] Delete Task");
         }
         static void readTasks(SQLiteConnection conn)
         {
@@ -48,7 +65,68 @@ namespace SQLReadingStuff
         }
         static void deleteTasks(SQLiteConnection conn)
         {
+            ConsoleKeyInfo choice;
+            do
+            {
+                deleteTasksMenu();
+                choice = Console.ReadKey();
+                Console.WriteLine();
+            } while (choice.Key != ConsoleKey.D1 || choice.Key != ConsoleKey.D2);
 
+            switch (choice.Key)
+            {
+                case ConsoleKey.D1:
+
+
+                    break;
+                case ConsoleKey.D2:
+                    ConsoleKeyInfo sureChoice;
+                    do
+                    {
+                        Console.WriteLine("Are you SURE? Y/N");
+                        sureChoice = Console.ReadKey();
+                        Console.WriteLine();
+                    } while (sureChoice.Key != ConsoleKey.Y || sureChoice.Key != ConsoleKey.N);
+                    if (sureChoice.Key == ConsoleKey.N)
+                    {
+                        Console.WriteLine("Good choice.");
+                        mainMenu(conn);
+                    }
+                    SQLiteCommand deleteACMD = new SQLiteCommand("DELETE FROM WeeklyAllocated;");
+                    deleteACMD.ExecuteNonQuery();
+                    Console.WriteLine("Delete everything.");
+                    break;
+                default:
+                    Console.WriteLine("no idea how you've done this lol."); // easter egg, impossible to get to though. lol.
+                    Thread.Sleep(5000);
+                    Console.WriteLine("you're still here? well... okay... bye bye");
+                    Thread.Sleep(1000);
+                    deleteTasks(conn);
+                    break;
+            }
+            mainMenu(conn);
+        }
+
+        static void deleteTasksMenu()
+        {
+            Console.WriteLine("[1] Delete Specific Tasks?");
+            Console.WriteLine("(Not Functional Yet)");
+            Console.WriteLine("[2] Delete Everything?");
+        }
+
+        static string daySelector()
+        {
+            
+            return null;
+        }
+        static void necessitiesTaskAutoSetup(SQLiteConnection conn)
+        {
+            for(int i = 1; i < 8; i++)
+            {
+                string theCMD = "INSERT INTO WeeklyAllocated VALUES (4, 'Sleep', 8, FALSE," + i + ");";
+                SQLiteCommand cmd = new SQLiteCommand(theCMD, conn);
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
