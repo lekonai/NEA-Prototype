065a8d09f7d075660fcc7f70053da69ba89d1a9e
23OC0183 <lifeaddlife@gmail.com>
2024-06-20 17:22:54 +0100

    commit check.

--- SQLReadingStuff/Program.cs
+++ SQLReadingStuff/Program.cs
@@ -18,7 +18,13 @@ namespace SQLReadingStuff
              *
             */
             SQLiteConnection conn = CreateConn();
-
+            ConsoleKeyInfo choiceK;
+            do
+            {
+                getMenuChoice();
+                choiceK = Console.ReadKey();
+                Console.WriteLine();
+            } while (choiceK.Key != ConsoleKey.D1 || choiceK.Key != ConsoleKey.D2 || choiceK.Key != ConsoleKey.D3);
         }
         static SQLiteConnection CreateConn()
         {
@@ -26,6 +32,23 @@ namespace SQLReadingStuff
             theConnection = new SQLiteConnection("Data Source = TasksFilled.db; Version = 3; New = True; Compress = True;"); // MAKE SURE TO CHANGE NAME HERE. tasksfilled is a placeholder name
             theConnection.Open();
             return theConnection;
+        }
+        static void getMenuChoice()
+        {
+            Console.Clear();
+            Console.WriteLine("Read [1]\nNew Task[2]\nDelete Task[3]");
+        }
+        static void readTasks(SQLiteConnection conn)
+        {
+
+        }
+        static void createTask(SQLiteConnection conn)
+        {
+
+        }
+        static void deleteTasks(SQLiteConnection conn)
+        {
+
         }
     }
 }
