ba3fe30b2e94bfac332872b3196d30c90a047ca9
lekonai <lekonai@users.noreply.github.com>
2024-06-30 17:40:15 +0100

    deleting tasks implemented.

--- SQLReadingStuff/Program.cs
+++ SQLReadingStuff/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity.ModelConfiguration.Conventions;
+﻿using System.Data;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Data.SQLite;
 using System.Diagnostics;
 using System.Net;
@@ -18,7 +19,7 @@ namespace SQLReadingStuff
 
             // i can't be asking this every time. the database has its limits and it can be violated and abused through this !
 
-            if (readTXT() == "false")
+            if (ReadTxt() == "false")
             {
                 ConsoleKeyInfo autoChoice;
                 do
@@ -29,16 +30,16 @@ namespace SQLReadingStuff
                 } while (autoChoice.Key != ConsoleKey.Y && autoChoice.Key != ConsoleKey.N); // auto fill database. this is just a filler really.
                 if (autoChoice.Key == ConsoleKey.Y)
                 {
-                    necessitiesTaskAutoSetup(conn);
-                    writeTXT("true");
+                    NecessitiesTaskAutoSetup(conn);
+                    WriteTxt("true");
                 }
             }
             else { }
             
-            mainMenu(conn);
+            MainMenu(conn);
         }
 
-        static void writeTXT(string toWrite) // wriites over text. complete
+        static void WriteTxt(string toWrite) // writes over text, used for true/false nothing else. complete
         {
             using (StreamWriter sw = new StreamWriter("autoF.txt", false))
             {
@@ -46,7 +47,7 @@ namespace SQLReadingStuff
             }
         }
 
-        static string readTXT() // reads file, return output. that's it // complete.
+        static string ReadTxt() // reads file, return output. that's it // complete.
         {
             string line = "";
             using (StreamReader sr = new StreamReader("autoF.txt"))
@@ -58,27 +59,27 @@ namespace SQLReadingStuff
             }
             return line;
         }
-        static void mainMenu(SQLiteConnection conn) // complete; it's a simple main menu!
+        static void MainMenu(SQLiteConnection conn) // complete; it's a simple main menu!
         {
             ConsoleKeyInfo choiceK;
             do
             {
-                getMenuChoice();
+                GetMenuChoice();
                 choiceK = Console.ReadKey();
                 Console.WriteLine();
             } while (choiceK.Key != ConsoleKey.D1 && choiceK.Key != ConsoleKey.D2 && choiceK.Key != ConsoleKey.D3);
             Console.WriteLine();
             if (choiceK.Key == ConsoleKey.D1)
             {
-                readTasks(conn);
+                ReadTasks(conn);
             }
             else if (choiceK.Key == ConsoleKey.D2)
             {
-                createTask(conn);
+                CreateTask(conn);
             }
             else if (choiceK.Key == ConsoleKey.D3)
             {
-                deleteTasks(conn);
+                DeleteTasks(conn);
             }
         }
         static SQLiteConnection CreateConn() // complete; all this does is create and open a connection !
@@ -88,12 +89,12 @@ namespace SQLReadingStuff
             theConnection.Open();
             return theConnection;
         }
-        static void getMenuChoice() // complete, it's just a WL
+        static void GetMenuChoice() // complete, it's just a WL
         {
             Console.Clear();
             Console.WriteLine("[1] Read\n[2] New Task\n[3] Delete Task"); // just cwl
         }
-        static void readTasks(SQLiteConnection conn) // complete, does as its asked.
+        static void ReadTasks(SQLiteConnection conn) // complete, does as its asked.
         {
             // it's gotta loop like 7 times
             Console.WriteLine();
@@ -103,8 +104,8 @@ namespace SQLReadingStuff
             {
                 for (int i = 0; i < 8; i++) // it's probably easier to just put in "if i == 1" output monday or something but an sqlite command would be easier.
                 {
-                    dayNameGet(conn, i); // just outputs the day
-                    taskGetAllDays(conn, i); // gets all the tasks of current day in loop
+                    DayNameGet(conn, i); // just outputs the day
+                    TaskGetAllDays(conn, i); // gets all the tasks of current day in loop
                     Console.WriteLine();
                 }
             }
@@ -125,32 +126,32 @@ namespace SQLReadingStuff
                 switch (choosingDay.Key)
                 {
                     case ConsoleKey.D0:
-                        taskIndividualDayDisplay(conn, 0); break;
+                        TaskIndividualDayDisplay(conn, 0); break;
                     case ConsoleKey.D1:
-                        taskIndividualDayDisplay(conn, 1); break;
+                        TaskIndividualDayDisplay(conn, 1); break;
                     case ConsoleKey.D2:
-                        taskIndividualDayDisplay(conn, 2); break;
+                        TaskIndividualDayDisplay(conn, 2); break;
                     case ConsoleKey.D3:
-                        taskIndividualDayDisplay(conn, 3); break;
+                        TaskIndividualDayDisplay(conn, 3); break;
                     case ConsoleKey.D4:
-                        taskIndividualDayDisplay(conn, 4); break;
+                        TaskIndividualDayDisplay(conn, 4); break;
                     case ConsoleKey.D5:
-                        taskIndividualDayDisplay(conn, 5); break;
+                        TaskIndividualDayDisplay(conn, 5); break;
                     case ConsoleKey.D6:
-                        taskIndividualDayDisplay(conn, 6); break;
+                        TaskIndividualDayDisplay(conn, 6); break;
                     case ConsoleKey.D7:
-                        taskIndividualDayDisplay(conn, 7); break;
+                        TaskIndividualDayDisplay(conn, 7); break;
                 } // probably could stuff this into a subroutine, but it's not THAT necessary
             }
             else
             {
-                readTasks(conn); // hell yeah recursion !
+                ReadTasks(conn); // hell yeah recursion !
             }
             Console.WriteLine("Back to main menu? Y/N"); // return to main menu, self explanatory.
             ConsoleKeyInfo mmChoice = Console.ReadKey();
             if (mmChoice.Key == ConsoleKey.Y)
             {
-                mainMenu(conn);
+                MainMenu(conn);
             }
             else
             {
@@ -158,7 +159,7 @@ namespace SQLReadingStuff
             }
         }
 
-        static void taskGetAllDays(SQLiteConnection conn, int i) // need this function for both the reading one and the one that isn't reading (the create tasks one)
+        static void TaskGetAllDays(SQLiteConnection conn, int i) // need this function for both the reading one and the one that isn't reading (the create tasks one)
         {
             // this has to contain both group order, and also print all metadata essentially
             // concisely speaking, it must be expandable !
@@ -178,7 +179,7 @@ namespace SQLReadingStuff
             }
             Console.WriteLine("====== [END OF TASKS FOR THIS DAY] ======");
         }
-        static void dayNameGet(SQLiteConnection conn, int i) // gets day name from sqlite table directly. that's it
+        static void DayNameGet(SQLiteConnection conn, int i) // gets day name from sqlite table directly. that's it
         {
             string dayCMD = "SELECT DayName FROM DaySpecificKey WHERE DayID = " + i + ";";
             SQLiteCommand dayGetCMD = new SQLiteCommand(dayCMD, conn);
@@ -186,11 +187,11 @@ namespace SQLReadingStuff
             {
                 while (dayRead.Read())
                 {
-                    Console.WriteLine("====== {0} ======",dayRead.GetString(0).ToUpper());
+                    Console.WriteLine("====== {0} ======",dayRead.GetString(0).ToUpper()); // make it look fancy with those equals
                 }
             }
         }
-        static void createTask(SQLiteConnection conn)
+        static void CreateTask(SQLiteConnection conn)
         {
             // DayID PK in DaySpecificKey
             // TaskDay FK in WeeklyAllocated
@@ -202,21 +203,17 @@ namespace SQLReadingStuff
             // 1 - School, 2 - Hobbies, 3 - Cadets, 4 - Necessary
             // 1/3/4 is an example of grouping etc; it will be autofilled in with the subroutine
             Console.WriteLine("What day would you like to put a task in?\n[0] Not Time Specific\n[1] Monday\n[2] Tuesday\n[3] Wednesday\n[4] Thursday\n[5] Friday\n[6] Saturday\n[7] Sunday");
-            int dayChoice = int.Parse(Console.ReadLine()); // yeah i don't care about making sure inputs work
-            if (dayChoice is > 7 or < 0)
-            {
-                createTask(conn); // recursion !
-            }
+            int dayChoice = DayChoice(); // implemented function, removed previous int.parse, cr and recursion. 
             Console.WriteLine();
             Console.WriteLine("Tasks For:");
-            dayNameGet(conn,dayChoice);
+            DayNameGet(conn,dayChoice);
             Console.WriteLine();
-            int usableTime = 24 - taskIndividualDayDisplay(conn, dayChoice);
+            int usableTime = 24 - TaskIndividualDayDisplay(conn, dayChoice);
             if (usableTime == 0)
             {
                 Console.WriteLine("Yeah, you're out of time for this day :/ Returning to main menu on keypress...");
                 Console.ReadKey();
-                mainMenu(conn);
+                MainMenu(conn);
             }
             Console.WriteLine();
             int timeLengthChoice = 0;
@@ -322,7 +319,7 @@ namespace SQLReadingStuff
             Console.WriteLine();
             if (isItRight.Key == ConsoleKey.N)
             {
-                createTask(conn);
+                CreateTask(conn);
             }
 
             string customTaskCreate =
@@ -334,7 +331,7 @@ namespace SQLReadingStuff
             ConsoleKeyInfo returnn = Console.ReadKey();
             if (returnn.Key == ConsoleKey.Y)
             {
-                mainMenu(conn);
+                MainMenu(conn);
             }
             else
             {
@@ -346,7 +343,7 @@ namespace SQLReadingStuff
             // VARIABLE THREE ! TIME SPECIFIC (tSpecific)
         }
         
-        static int taskIndividualDayDisplay(SQLiteConnection conn, int day)
+        static int TaskIndividualDayDisplay(SQLiteConnection conn, int day)
         {
             // takes in day, displays that day's tasks
             // additionally returns the hours you can allocate
@@ -364,13 +361,18 @@ namespace SQLReadingStuff
             
             return totalTime;
         }
-        
-        static void deleteTasks(SQLiteConnection conn)
+
+        public struct IDCollection
+        {
+            public string name; // TaskName in WeeklyAllocated
+            public int hours; // get the task's hours
+        }
+        static void DeleteTasks(SQLiteConnection conn)
         {
             ConsoleKeyInfo choice;
             do // works
             {
-                deleteTasksMenu();
+                DeleteTasksMenu();
                 choice = Console.ReadKey();
                 Console.WriteLine();
             } while (choice.Key != ConsoleKey.D1 && choice.Key != ConsoleKey.D2);
@@ -378,8 +380,59 @@ namespace SQLReadingStuff
             switch (choice.Key)
             {
                 case ConsoleKey.D1: // this one has to be specific with what it deletes... very complex.
-                    deleteTasks(conn); // TO REMOVE, THIS IS INCOMPLETE.
+                    Console.WriteLine("\n=============\n");
+                    Console.WriteLine("What day are you picking?");
+                    for (int i = 0; i < daysKey.Length; i++)
+                    {
+                        Console.WriteLine("[{0}] {1}", i, daysKey[i]); // display days
+                    }
+                    Console.WriteLine("\n=============\n");
+                    int currentDayChoice = DayChoice(); // get day choice
+                    Console.WriteLine("You've chosen \nHere are the tasks for that day:");
+                    // i can't use task individual day display because i need a bit more info than that. but i can take it's sql query.
+                    int iteratorCounter = 0;
+                    List<IDCollection> tasksToSelect = new List<IDCollection>();
+                    string theHereCMD = "SELECT Name, TaskTimeHours, TaskName, TimeSpecific FROM TaskType, DaySpecificKey, WeeklyAllocated WHERE WeeklyAllocated.TaskDay = " + currentDayChoice + " AND DaySpecificKey.DayID = WeeklyAllocated.TaskDay AND  WeeklyAllocated.TaskGroup = TaskType.ID;";
+                    SQLiteCommand dayIterator = new SQLiteCommand(theHereCMD, conn);
+                    using (SQLiteDataReader reader = dayIterator.ExecuteReader()) // i realise it'd make more sense using the older subroutine which gets all the tasks, and remove the iteration so the iteration is only used when necessary but i'm not refactoring right now.
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("[SELECT THIS TASK WITH: {0}]", iteratorCounter); // iterator counter has no purpose but to just give everything an ID in this subcontext.
+                            Console.WriteLine("Task: {0}\nCategory: {1}\nTime: {2}", reader.GetString(2), reader.GetString(0), reader.GetInt32(1));
+                            Console.WriteLine();
+                            IDCollection temp;
+                            temp.name = reader.GetString(2);
+                            temp.hours = reader.GetInt32(1);
+                            tasksToSelect.Add(temp);
+                            iteratorCounter++;
+                        }
+                    }
 
+                    if (iteratorCounter == 0)
+                    {
+                        Console.WriteLine("No tasks to delete here");
+                        Thread.Sleep(2500);
+                        Console.WriteLine("Returning to main menu on keypress...");
+                        Console.ReadKey();
+                        MainMenu(conn);
+                    }
+                    
+                    // i could probably break this down into loads of funcs and stuff but it is too late
+                    int select;
+                    do
+                    {
+                        Console.WriteLine("Choose your task from 0 to {0}", iteratorCounter - 1);
+                        select = int.Parse(Console.ReadLine());
+                    } while (select < 0 || select > iteratorCounter - 1);
+                    Console.WriteLine();
+                    Console.WriteLine("You're going to delete the task {0} which you've dedicated {1} hours to on {2}", tasksToSelect[select].name, tasksToSelect[select].hours, daysKey[select]);
+                    string deleteSpecificTaskCMD = "DELETE FROM WeeklyAllocated WHERE WeeklyAllocated.TaskDay = " + currentDayChoice + " AND WeeklyAllocated.TaskName = '" + tasksToSelect[select].name + "' AND WeeklyAllocated.TaskTimeHours = " + tasksToSelect[select].hours + ";";
+                    // full cmd here noted for error checking
+                    SQLiteCommand deleter = new SQLiteCommand(deleteSpecificTaskCMD, conn);
+                    
+                    deleter.ExecuteNonQuery();
+                    
                     break;
                 case ConsoleKey.D2: // delete everything !
                     ConsoleKeyInfo sureChoice;
@@ -392,11 +445,11 @@ namespace SQLReadingStuff
                     if (sureChoice.Key == ConsoleKey.N)
                     {
                         Console.WriteLine("Good choice.");
-                        mainMenu(conn);
+                        MainMenu(conn);
                     }
                     SQLiteCommand deleteACMD = new SQLiteCommand("DELETE FROM WeeklyAllocated;", conn);
                     deleteACMD.ExecuteNonQuery();
-                    writeTXT("false");
+                    WriteTxt("false");
                     Console.WriteLine("Deleted everything.");
                     break;
                 default: // this is impossible to get to i thought it'd be funny to have it please disregard it !
@@ -404,22 +457,58 @@ namespace SQLReadingStuff
                     Thread.Sleep(5000);
                     Console.WriteLine("you're still here? well... okay... bye bye");
                     Thread.Sleep(1000);
-                    deleteTasks(conn);
+                    DeleteTasks(conn);
                     break;
             }
-            mainMenu(conn);
+
+            Console.WriteLine("Returning to Main Menu...");
+            Thread.Sleep(6500);
+            MainMenu(conn);
         }
 
+        static int DayChoice() // Complete... just get's the day choice. might implement this into other things.
+        {
+            ConsoleKeyInfo theC;
+            Console.WriteLine("Please Enter Your Choice:");
+            Console.WriteLine();
+            theC = Console.ReadKey();
+            Console.WriteLine();
+            if (theC.Key is not (ConsoleKey.D0 or ConsoleKey.D1 or ConsoleKey.D2 or ConsoleKey.D3 or ConsoleKey.D4
+                or ConsoleKey.D5
+                or ConsoleKey.D6 or ConsoleKey.D7))
+            {
+                DayChoice();
+            }
+            switch (theC.Key)
+            {
+                case ConsoleKey.D0:
+                    return 0;
+                case ConsoleKey.D1:
+                    return 1;
+                case ConsoleKey.D2:
+                    return 2;
+                case ConsoleKey.D3:
+                    return 3;
+                case ConsoleKey.D4:
+                    return 4;
+                case ConsoleKey.D5:
+                    return 5;
+                case ConsoleKey.D6:
+                    return 6;
+                case ConsoleKey.D7:
+                    return 7;
+            }
+
+            return 0;
+        }
         
-        
-        static void deleteTasksMenu() // it's just a menu lol
+        static void DeleteTasksMenu() // it's just a menu lol
         {
             Console.WriteLine("[1] Delete Specific Tasks?");
-            Console.WriteLine("(This won't actually do anything yet)"); // TO REMOVE, THIS ISN'T COMPLETE.
             Console.WriteLine("[2] Delete Everything?");
         }
         
-        static void necessitiesTaskAutoSetup(SQLiteConnection conn) // complete.
+        static void NecessitiesTaskAutoSetup(SQLiteConnection conn) // complete.
         {
             for(int i = 1; i < 8; i++)
             {
